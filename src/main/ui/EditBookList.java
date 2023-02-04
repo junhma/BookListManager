1e8b3c154c0ad07e60c979304c7e700080bdbbe6
Jun <i9l2b@ugrad.cs.ubc.ca>
2023-02-03 19:35:38 -0800

    added while loops for modifying multiple books

--- src/main/ui/EditBookList.java
+++ src/main/ui/EditBookList.java
@@ -11,17 +11,22 @@ public class EditBookList {
     // MODIFIES: the book list
     // EFFECTS: add a book to the book list
     public void addBooksUI(BookList bookList, Scanner in) {
+        String replyContinue;
         System.out.println("Do you want to add books? Yes/No");
         String replyAdd = in.nextLine();
-
         if (replyAdd.equals("Yes")) {
-            System.out.println("Enter the name of the book you want to add.");
-            String title = in.nextLine();
-            System.out.println("Enter the current chapter of the book you want to add.");
-            int chapter = Integer.parseInt(in.nextLine());
+            do {
+                System.out.println("Enter the name of the book you want to add.");
+                String title = in.nextLine();
+                System.out.println("Enter the current chapter of the book you want to add.");
+                int chapter = Integer.parseInt(in.nextLine());
 
-            Book addedBook = new Book(title, chapter);
-            bookList.addBook(addedBook);
+                Book addedBook = new Book(title, chapter);
+                bookList.addBook(addedBook);
+
+                System.out.println("Continue? Yes/No");
+                replyContinue = in.nextLine();
+            } while (replyContinue.equals("Yes"));
         }
     }
 
@@ -29,19 +34,25 @@ public class EditBookList {
     // MODIFIES: the book list
     // EFFECTS: add a book to the book list
     public void removeBooksUI(BookList bookList, Scanner in) {
+        String replyContinue;
+
         System.out.println("Do you want to remove books? Yes/No");
         String replyRemove = in.nextLine();
 
         if (replyRemove.equals("Yes")) {
-            System.out.println("Choose a book to remove.");
-            for (int i = 0; i < bookList.getBookListLength(); i++) {
-                System.out.println(i + "" + bookList.getBookList().get(i).getTitle());
-            }
+            do {
+                System.out.println("Choose a book to remove.");
+                for (int i = 0; i < bookList.getBookListLength(); i++) {
+                    System.out.println(i + " " + bookList.getBookList().get(i).getTitle());
+                }
 
-            int choice = Integer.parseInt(in.nextLine());
+                int choice = Integer.parseInt(in.nextLine());
+                Book removedBook = bookList.getBookList().get(choice);
+                bookList.removeBook(removedBook);
 
-            Book removedBook = bookList.getBookList().get(choice);
-            bookList.removeBook(removedBook);
+                System.out.println("Continue? Yes/No");
+                replyContinue = in.nextLine();
+            } while (replyContinue.equals("Yes"));
         }
     }
 
@@ -49,23 +60,30 @@ public class EditBookList {
     // MODIFIES: the book list
     // EFFECTS: see and edit the number of chapters for a given book
     public void changeChaptersUI(BookList bookList, Scanner in) {
+        String replyContinue;
+
         System.out.println("Do you want to change current chapter of a book? Yes/No");
         String replyChangeChapters = in.nextLine();
 
         if (replyChangeChapters.equals("Yes")) {
-            System.out.println("Choose a book.");
-            for (int i = 0; i < bookList.getBookListLength(); i++) {
-                System.out.println(i + " " + bookList.getBookList().get(i).getTitle());
-            }
+            do {
+                System.out.println("Choose a book.");
+                for (int i = 0; i < bookList.getBookListLength(); i++) {
+                    System.out.println(i + " " + bookList.getBookList().get(i).getTitle());
+                }
 
-            int choice = Integer.parseInt(in.nextLine());
+                int choice = Integer.parseInt(in.nextLine());
 
-            Book editedBook = bookList.getBookList().get(choice);
-            System.out.println("You are currently at chapter " + editedBook.getChapter());
+                Book editedBook = bookList.getBookList().get(choice);
+                System.out.println("You are currently at chapter " + editedBook.getChapter());
 
-            System.out.println("Enter the new number of chapters.");
-            int numChapter = Integer.parseInt(in.nextLine());
-            editedBook.changeChapter(numChapter);
+                System.out.println("Enter the new number of chapters.");
+                int numChapter = Integer.parseInt(in.nextLine());
+                editedBook.changeChapter(numChapter);
+
+                System.out.println("Continue? Yes/No");
+                replyContinue = in.nextLine();
+            } while (replyContinue.equals("Yes"));
         }
     }
 }
